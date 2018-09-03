8333d3f153f56cd0a0664fb9bfd5dae6e643b593
leandersonandre <leanderson.andre@gmail.com>
2018-09-03 16:35:31 -0300

    Adicionado exemplo de tableview com botao.

---
+++ src/br/com/leandersonandre/javafx/exemplos/layout/tableviewcombotao/Controller.java
@@ -0,0 +1,74 @@
+package br.com.leandersonandre.javafx.exemplos.layout.tableviewcombotao;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    public TableView tableView;
+
+
+    private final ObservableList<User> data =
+            FXCollections.observableArrayList(
+                    new User(1, "User a","surname a", "a@example.com"),
+                    new User(2, "User b", "surname b","b@example.com"),
+                    new User(3, "User c", "surname c","c@example.com"),
+                    new User(4, "User d", "surname d","d@example.com"),
+                    new User(5, "User e", "surname e","e@example.com")
+            );
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tableView.setItems(data);
+        addButtonToTable();
+
+    }
+    private void addButtonToTable() {
+        TableColumn<User, Void> colBtn = new TableColumn("");
+
+        Callback<TableColumn<User, Void>, TableCell<User, Void>> cellFactory = new Callback<TableColumn<User, Void>, TableCell<User, Void>>() {
+            @Override
+            public TableCell<User, Void> call(final TableColumn<User, Void> param) {
+                final TableCell<User, Void> cell = new TableCell<User, Void>() {
+
+                    private final Button btn = new Button("Action");
+
+                    {
+                        btn.setOnAction((ActionEvent event) -> {
+                            User data = getTableView().getItems().get(getIndex());
+                            System.out.println("selectedData: " + data);
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+
+        colBtn.setCellFactory(cellFactory);
+
+        tableView.getColumns().add(colBtn);
+
+    }
+}
---
+++ src/br/com/leandersonandre/javafx/exemplos/layout/tableviewcombotao/Main.java
@@ -0,0 +1,29 @@
+package br.com.leandersonandre.javafx.exemplos.layout.tableviewcombotao;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        // Carrega a Tela
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("br/com/leandersonandre/javafx/exemplos/layout/tableviewcombotao/tela.fxml"));
+        // Atribui o título da Janela
+        primaryStage.setTitle("Exemplo de Tabela com botao");
+        // Atribui o tamanho da Janela
+        primaryStage.setScene(new Scene(root));
+        // Exibe a Janela
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        // Método para inicializar a tela
+        launch(args);
+    }
+}
